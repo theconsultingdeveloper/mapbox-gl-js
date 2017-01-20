961d30155c77812faa83bccdf94a2eb9d1129543
Lucas Wojciechowski <lucas@mapbox.com>
2017-01-20 15:44:08 -0800

    Add a build tests to ensure only the latest minified style spec reference JSON is pulled into the minified bundle

--- js/ui/camera.js
+++ js/ui/camera.js
@@ -327,8 +327,8 @@ class Camera extends Evented {
             nw = tr.project(bounds.getNorthWest()),
             se = tr.project(bounds.getSouthEast()),
             size = se.sub(nw),
-            scaleX = (tr.width - options.padding * 2 - Math.abs(offset.x) * 2) / size.x,
-            scaleY = (tr.height - options.padding * 2 - Math.abs(offset.y) * 2) / size.y;
+            scaleX = (tr.width - options.padding.left - options.padding.right - Math.abs(offset.x) * 2) / size.x,
+            scaleY = (tr.height - options.padding.top - options.padding.bottom - Math.abs(offset.y) * 2) / size.y;
 
         options.center = tr.unproject(nw.add(se).div(2));
         options.zoom = Math.min(tr.scaleZoom(tr.scale * Math.min(scaleX, scaleY)), options.maxZoom);
